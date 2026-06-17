cb49baf37d6077ba1ef07f4fb0af2d38c35ce084
hitesh42 <42150806+hitesh42@users.noreply.github.com>
2026-06-17 15:41:01 +0530

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res = "";
+        for(int i=0;i<strs.size();i++){
+            res+= to_string(strs[i].length()) + "#" +strs[i];
+        }
+        // cout << res << endl;
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        // stringstream str(s);
+        // string token;
+
+        int i=0;
+        while(i<s.length()){
+            int j = s.find('#', i);
+            int count = stoi(s.substr(i,j-i));
+            j = j+1;
+            res.push_back(s.substr(j, count));
+            i = j+count;
+        }
+
+        // while(getline(str, token,',')){
+        //     res.push_back(token);
+        //     cout << token << "h";
+        // }
+        return res;
+    }
+};
